41cb7101afbc0e7b9fb5ecd3789e467b0c7cecd4
yadid ramot <e_yadid@hotmail.com>
2025-11-13 22:17:50 -0500

    Test homepage

--- tests/homepage.spec.js
+++ tests/homepage.spec.js
@@ -2,7 +2,7 @@
 import { test, expect } from '@playwright/test';
 
 test('everyset has title', async ({ page }) => {
-  await page.goto('https://https://www.everyset.com//');
+  await page.goto('https://www.everyset.com//');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/The source of truth for background on every set/);
